978aa1722450c22ff6dacf81fa9423b39cbd1361
Fgimaret <florent.gimaret@gmail.com>
2021-08-28 22:25:44 +0200

    Final commit

--- lib/main.dart
+++ lib/main.dart
@@ -99,7 +99,11 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _deleteTransaction() {}
+  void _deleteTransaction(String id) {
+    setState(() {
+      _userTransactions.removeWhere((tx) => tx.id == id);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -121,7 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Chart(_recentTransactions),
-            TransactionList(_userTransactions),
+            TransactionList(_userTransactions, _deleteTransaction),
           ],
         ),
       ),
